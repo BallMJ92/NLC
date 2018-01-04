a94d49d0fc8bb9040d919a4cb59b7dbce8c83885
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-01-04 15:03:25 +0000

    Update processor.py

--- processor.py
+++ processor.py
@@ -11,34 +11,49 @@ class processor:
 
         TAPrTV - Transitive Active Present Tense Verbs
         TAPaTV - Transitive Active Past Tense Verbs
-
+        PPr - Personal Pronouns
+        SPr - Subjective Pronouns
         """
 
         indexLocations = []
         keyWords = []
-        x = 0
         n = 1
 
         for i in range(0, len(var)):
-            if var[x] in transitiveActivePresentTenseVerbs:
-                print("Transitive active present tense verb '%s' is word %d within the sentence." % (str(var[x]), n))
-                indexLocations.append(int(x))
-                keyWords.append(TAPrTV+var[x])
-            if var[x] in transitiveActivePastTenseVerbs:
-                print("Transitive active past tense verb '%s' is word %d within the sentence." % (str(var[x]), n))
-                indexLocations.append(int(x))
-                keyWords.append(TAPaTV+var[x])
-            x+=1
+            if var[i] in transitiveActivePresentTenseVerbs:
+                print("Transitive active present tense verb '%s' is word %d within the sentence." % (str(var[i]), n))
+                indexLocations.append(int(i))
+                index = int(transitiveActivePresentTenseVerbs.index(var[i]))
+                print("Opposite of %s is %s" % (var[i], transitiveActivePastTenseVerbs[index]))
+                keyWords.append(TAPrTV+var[i])
+            if var[i] in transitiveActivePastTenseVerbs:
+                print("Transitive active past tense verb '%s' is word %d within the sentence." % (str(var[i]), n))
+                indexLocations.append(int(i))
+                index = int(transitiveActivePastTenseVerbs.index(var[i]))
+                print("Opposite of %s is %s" % (var[i], transitiveActivePresentTenseVerbs[index]))
+                keyWords.append(TAPaTV+var[i])
+            if var[i] in personalPronouns:
+                print("Personal pronoun '%s' is word %d within the sentence." % (str(var[i]), n))
+                indexLocations.append(int(i))
+                index = int(personalPronouns.index(var[i]))
+                keyWords.append(PPr+var[i])
+            if var[i] in subjectivePronouns:
+                print("Subjective pronoun '%s' is word %d within the sentence." % (str(var[i]), n))
+                indexLocations.append(int(i))
+                index = int(subjectivePronouns.index(var[i]))
+                keyWords.append(SPr+var[i])
             n+=1
 
         print(keyWords)
-
 
     def main(self):
         sentence = str(input("Enter a sentence: "))
         self.wordSplit(sentence)
         self.sentenceComprehension(self.wordList)
 
+
 if __name__ == "__main__":
     nl = processor()
     nl.main()
+
+
